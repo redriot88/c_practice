63f179f492fdc742083e77590b656c5959ee171a
redriot88 <166695662+redriot88@users.noreply.github.com>
2025-07-09 18:30:54 +1000

    Create anagram.cpp
    created 1st rendition of anagram finder

---
+++ anagram.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+  int counter1[] = {0,0,0,0};
+  int counter2[] = {0,0,0,0};
+
+  char s1[] = "dbb cccccaacb cdbababdcdcdab dcdad";
+  char s2[] = "bbbcc bdddccccad cdbbaaacaccdabdd";
+
+  for (int i = 0; i < strlen(s1); i++){
+    if (s1[0] == 'a'){
+      counter1[0]++;
+    }
+    if (s1[1] == 'b'){
+      counter1[1]++;
+    }
+    if (s1[2] == 'c'){
+      counter1[2]++;
+    }
+    if (s1[3] == 'd'){
+      counter1[3]++;
+    } 
+    if (s1[i] == ' '){
+      continue;
+    }
+  for (int j = 0; j < strlen(s2); j++){
+    if (s2[0] == 'a'){
+      counter2[0]++;
+    }
+    if (s2[1] == 'b'){
+      counter2[1]++;
+    }
+    if (s2[2] == 'c'){
+      counter2[2]++;
+    }
+    if (s2[3] == 'd'){
+      counter2[3]++;
+    }
+    if (s2[j] == ' '){
+      continue;
+    }
+  }
+  int flag = 0;
+  for (int k = 0; k < 4; k++){
+    if (counter1[k] != counter2[k]){
+      flag = 1;
+      printf("Not Anagram!");
+      break;
+    }
+    if (flag == 0){
+      printf("Anagram!");
+    }
+  }
+  }
+}
